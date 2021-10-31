917797237a1a4bd98a1f91815071335ee533283d
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-31 16:46:54 +0530

    add: added custom types and arrays for mapper

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import styles from '../styles/components/Input.module.scss';
 
-const Input: React.FC = () => {
-  return <></>
+interface InputProps{
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<InputProps> = (props) => {
+  return <div className={styles.InputContainer}>
+    <label className={styles.InputLabel}>{props.placeholder}</label>
+    <input className={styles.Input} type={props.type} value={props.value} onChange={props.onChange} />
+  </div>
 }
 
 export default Input;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Input from '../components/Input';
 import styles from '../styles/pages/Signup.module.scss'
 
 interface SignupStateProps{
@@ -9,8 +10,21 @@ interface SignupStateProps{
   source: "WEB_APP"
 }
 
+const inputArray = ["name", "email", "referral"];
+type FormInputs = "name" | "email" | "referral";
+
 const Signup: React.FC = () => {
-  const [signupData, setSignupData] = React.useState()
+  const [signupData, setSignupData] = React.useState<SignupStateProps>({
+    name: "",
+    email: "",
+    referral: "",
+    privacyPolicy: false,
+    source: "WEB_APP"
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    
+  }
 
   return <div className={styles.SignupContainer}>
     <section className={styles.HeaderContainer}>
@@ -18,7 +32,15 @@ const Signup: React.FC = () => {
       <hr/>
       <p>Please enter the following details</p>
     </section>
-    <section className={styles.InputContainer}></section>
+    <section className={styles.InputContainer}>
+
+       {
+          inputArray.map((input, index) => {
+             return  <Input key={index} placeholder={input} type="text" value={signupData[input as FormInputs]} onChange={handleChange}/>
+          })
+       }
+        
+    </section>
   </div>
 }
 
