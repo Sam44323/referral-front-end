32d8c117ebd394320ffddf4104f4efbc53d03115
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-31 20:40:31 +0530

    add: added common mixin stylings

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from '../styles/pages/Signin.module.scss'
 
 const Signin: React.FC = () => {
-  return <></>
+  return <div className={styles.SigninContainer}>
+     <section className={styles.HeaderContainer}>
+      <h1>Login</h1>
+    </section>
+  </div>
 }
 
 export default Signin;
