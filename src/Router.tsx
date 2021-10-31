9c6a7433170448ce7a18f596b2130bc60aa93c86
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-31 14:34:06 +0530

    add: added file for routing

---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Routes: FC = () => {
+  return (
+    <Router>
+      <Switch>
+        {/* <Route path="/bridge" component={</>} /> */}
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
