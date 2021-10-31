6673a7e13be0fe99eed03a11e0f79a2a4d18af31
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-31 14:38:30 +0530

    update: updated the routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,16 @@
 import { FC } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
 
 const Routes: FC = () => {
   return (
     <Router>
       <Switch>
-        {/* <Route path="/bridge" component={</>} /> */}
+        <Route path="/signup" component={Signup} />
+        <Route path="/signin" component={Signin} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </Router>
   );
