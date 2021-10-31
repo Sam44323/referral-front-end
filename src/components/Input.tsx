072fefde23b1d6c74e0476bd0f4687e11b786bea
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-31 16:05:44 +0530

    add: added component for input

---
+++ src/components/Input.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/components/Input.module.scss';
+
+const Input: React.FC = () => {
+  return <></>
+}
+
+export default Input;
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import styles from '../styles/pages/Signup.module.scss'
 
+interface SignupStateProps{
+  name: string,
+  email: string,
+  referral: string,
+  privacyPolicy: boolean,
+  source: "WEB_APP"
+}
+
 const Signup: React.FC = () => {
-  return <></>
+  const [signupData, setSignupData] = React.useState()
+
+  return <div className={styles.SignupContainer}>
+    <section className={styles.HeaderContainer}></section>
+    <section className={styles.InputContainer}></section>
+  </div>
 }
 
 export default Signup;
